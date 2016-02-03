b0510bdc0a8b10ea97be13cb8e775834e758eb58
Brian Leathem <bleathem@gmail.com>
2016-02-03 07:14:50 -0800

    Fixed js file globbing for the watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       js: {
-        files: ['<%= app.app %>/scripts/{,*/}*.js'],
+        files: ['<%= app.app %>/**/*.js'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: 35730
