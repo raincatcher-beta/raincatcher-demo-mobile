d8e50cf417d9a1058998654c215f2af5190bf475
Brian Leathem <bleathem@gmail.com>
2016-02-22 18:35:56 -0800

    Consolidated module init tasks according to RHMAP-4407

--- src/app/app.js
+++ src/app/app.js
@@ -109,7 +109,21 @@ angular.module('wfm-mobile', [
     })
 })
 
-.run(function($rootScope, $state, userClient) {
+.run(function($rootScope, $state, $q, mediator, userClient) {
+  var initPromises = [];
+  var initListener = mediator.subscribe('promise:init', function(promise) {
+    initPromises.push(promise);
+  });
+  mediator.publish('init');
+  console.log(initPromises.length, 'init promises to resolve.');
+  var all = (initPromises.length > 0) ? $q.all(initPromises) : $q.when(null);
+  all.then(function() {
+    $rootScope.ready = true;
+    console.log(initPromises.length, 'init promises resolved.');
+    mediator.remove('promise:init', initListener.id);
+    return null;
+  });
+
   $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
     if(toState.name !== "app.login"){
       userClient.hasSession().then(function(hasSession) {
