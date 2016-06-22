5e5503b363ae50558457c6049b9322bc7e22de4f
sebastien blanc <sblanc@localhost.localdomain>
2016-06-22 17:20:15 +0200

    removed license headers

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,3 @@
-/**
-* CONFIDENTIAL
-* Copyright 2016 Red Hat, Inc. and/or its affiliates.
-* This is unpublished proprietary source code of Red Hat.
-**/
 'use strict';
 
 var angular = require('angular');
